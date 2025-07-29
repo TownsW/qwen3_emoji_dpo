91acd15439de9f1e7c640050276d3d6a861b381c
TownsW <522087994@qq.com>
2025-07-30 06:52:38 +0800

    Update run_train_dpo_transformers.sh

--- train/run_train_dpo_transformers.sh
+++ train/run_train_dpo_transformers.sh
@@ -26,21 +26,21 @@ unset TORCH_HOME
 unset TRITON_CACHE_DIR
 unset HF_HOME
 
-pip uninstall -y apex
-pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com torchvision==0.18.0
-pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com  torch==2.3.0
-pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com /nfs-151/disk8/townswu/codes/VITA-main/script/whl_list/flash_attn-2.7.4.post1+cu12torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl --no-build-isolation
-pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com transformers==4.51.0
-pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com trl
+#pip uninstall -y apex
+#pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com torchvision==0.18.0
+#pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com  torch==2.3.0
+#pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com /nfs-151/disk8/townswu/codes/VITA-main/script/whl_list/flash_attn-2.7.4.post1+cu12torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl --no-build-isolation
+#pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com transformers==4.51.0
+#pip install -i http://pip.duxiaoman-int.com/root/pypi/+simple/  --trusted-host pip.duxiaoman-int.com trl
 
-pip uninstall apex -y
+#pip uninstall apex -y
 #cd /var/s3fs/wutong02/codes/apex-master
 #pip install -v --no-cache-dir --global-option=“–cpp_ext” --global-option=“–cuda_ext” ./
 #cd /home/storages/dev95/disk8/townswu/codes/InternVL-main/internvl_chat/
 #pip install apex
 pip list
 
-cd /nfs-151/disk8/townswu/codes/qwen3_emoji_dpo/
+#cd /nfs-151/disk8/townswu/codes/qwen3_emoji_dpo/
 BATCH_SIZE=4
 EPOCH=1
 GPUS=2
